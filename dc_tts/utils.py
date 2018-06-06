d52c534ee88d42c2f8bcf185a6b20c017d18599e
Kyubyong Park <longinglove@nate.com>
2018-06-06 21:24:06 +0900

    Update utils.py

--- dc_tts/utils.py
+++ dc_tts/utils.py
@@ -2,7 +2,7 @@
 #/usr/bin/python2
 '''
 By kyubyong park. kbpark.linguist@gmail.com. 
-https://www.github.com/kyubyong/multi-speech-corpora/dc_tts
+https://www.github.com/kyubyong/css10
 '''
 from __future__ import print_function, division
 
